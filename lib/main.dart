b7c2c4d17b77708c67beae1280b0e72703b7780f
sahana170498 <sahanammr4@gmail.com>
2022-12-07 15:58:31 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -126,6 +126,7 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 //added one line
+//added one line
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
